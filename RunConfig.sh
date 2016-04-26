959bbd19448a73df3c5211b8cedba49fec2f1fc8
Roddy Dairion <roddy.dairion@appletree.co.uk>
2016-04-26 15:20:51 +0100

    test

--- RunConfig.sh
+++ RunConfig.sh
@@ -1,28 +1,65 @@
 #!/bin/bash
+preConfig ()
+{
+	hostnameDisplay=hostname -f
+	echo -n "Enter Hostname (Leave blank if you do not wish to change current hostname. Current Hostname $hostnameDisplay): "
+	read text
+	if [ -z "$text"]
+	then
+		echo "Unchanged hostname"
+	else
+		hNAME="$text"
+	fi
 
-echo -n "Enter Hostname (Leave blank if you do not wish to change current hostname): "
+	echo -n "Enter path to DocumentRoot (/var/www/html/):"
+	read text
+	if [ -z "$text" ]
+	then
+		PATH="/var/www/html"
+	else
+		PATH="$text"
+	fi
+
+	while read -p "Enter domain name/project name (eg: example.com, project.example.com): " PROJECT && [[ -z "$PROJECT" ]] ; do
+		echo "You need to enter a domain name/project name!"
+	done
+
+	while read -p "Enter email address to be configured with project: " EMAIL && [[ -z "$EMAIL" ]] ; do
+		echo "You need to enter an email address!"
+	done
+
+	echo "Select an IP address to assign to domain name/project name: "
+	select ip in $(ifconfig | awk '/inet addr/{print substr($2,6)}'); do 
+	if [ "$ip" = "exit" ]
+	then
+		exit 0
+	elif [ -n "$ip" ]
+	then
+		echo $ip
+		break
+	else
+		echo "Invalid choice"
+	fi
+	done
+}
+
+writeConfig(){
+preConfig
+echo "Hostname            : $hName"
+echo "DocumentRoot        : $PATH"
+echo "Domain/Project name : $PROJECT"
+echo "Email               : $EMAIL"
+echo "IP Assigned         : $ip"
+
+echo -n "Are you sure you want to apply the configuration above (Y/n)?"
 read text
-if [ -z "$text"]
+if [ "${text^^}" = "Y"]
 then
-	echo "Unchanged hostname"
-else
-	echo "HOSTNAME=$text" >> /etc/sysconfig/network
-	hostname "$text"
-fi
+cat <<EOF >> /etc/hosts
+"$ip    $PROJECT"
+EOF
 
-echo -n "Enter path to DocumentRoot (/var/www/html/):"
-read text
-if [ -z "$text" ]
-then
-	path="/var/www/html"
-else
-	path="$text"
-fi
-
-while read -p "Enter domain name/project name (eg: example.com, project1.example.com): " PROJECT && [[ -z "$PROJECT" ]] ; do
-	echo "You need to enter a domain name/project name!"
-done
-echo $path/$PROJECT
+###Configuration of Apache Virtual Host
 file="/etc/httpd/conf.d/vhost.conf"
 
 if [ -f "$file" ]
@@ -30,20 +67,56 @@ then
   echo "$file Vhost file found."
 else
   echo "$file not found. Creating file."
-cat > /etc/httpd/conf.d/vhost.conf << EOF1
+cat <<EOF >> /etc/httpd/conf.d/vhost.conf
+
+#Creating config for Vhost $PROJECT
 <VirtualHost *:8080>
-     ServerAdmin roddy@orange.mu
-     ServerName "$path"
-     ServerAlias "$path"
-     DocumentRoot "$path/$PROJECT"/public_html/
-     ErrorLog /var/www/html/"$path"/logs/error.log
-     CustomLog /var/www/html/"$path"/logs/access.log combined
+     ServerAdmin $EMAIL
+     ServerName $PATH
+     ServerAlias $PATH
+     DocumentRoot $PATH/$PROJECT/public_html/
+     ErrorLog $PATH/$PROJECT/logs/error.log
+     CustomLog $PATH/$PROJECT/logs/access.log combined
      <Directory />
      AllowOverride All
      </Directory>
 </VirtualHost>
-EOF1
+EOF
 fi
+
+###Configuration of Nginx Virtual host
+file=" /etc/nginx/conf.d/virtual.conf"
+cat <<EOF >> /etc/httpd/conf.d/vhost.conf
+
+#Creating config for Vhost $PROJECT
+server {
+ listen 80;
+root $PATH/$PROJECT/public_html/;
+ index index.php index.html index.htm;
+server_name $PROJECT;
+location / {
+ try_files $uri $uri/ /index.php;
+ }
+location ~ \.php$ {
+
+ proxy_set_header X-Real-IP $remote_addr;
+ proxy_set_header X-Forwarded-For $remote_addr;
+ proxy_set_header Host $host;
+ proxy_pass http://127.0.0.1:8080;
+
+}
+
+location ~ /\.ht {
+ deny all;
+ }
+}
+EOF
+}
+elif [ "${text^^}" = "N"]
+then
+	preConfig
+fi
+
 #echo "You entered: $text"
 #nano /etc/httpd/conf.d/vhost.conf
 #<VirtualHost *:8080>
